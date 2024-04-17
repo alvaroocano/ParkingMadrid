71e1e91f099910320c8df3acc1dfb26452e67b8a
Álvaro Cano Sánchez <acanito2003@gmail.com>
2024-04-17 18:27:26 +0200

    Inicio de sesión con google

--- app/src/main/java/com/example/parkingmadrid/MainActivity.kt
+++ app/src/main/java/com/example/parkingmadrid/MainActivity.kt
@@ -1,46 +1,79 @@
-package com.example.parkingmadrid
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.parkingmadrid.ui.theme.ParkingMadridTheme
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.parkingmadrid.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ParkingMadridTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        setContentView(R.layout.main_activity)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        val signInButton = findViewById<Button>(R.id.buttonLoginGoogle)
+        signInButton.setOnClickListener {
+            signInWithGoogle()
+        }
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
+
+    private fun signInWithGoogle() {
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Autenticar con Google
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Manejar errores
+                Log.w(TAG, "Google sign in failed", e)
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Inicio de sesión exitoso
+                    val user = mAuth.currentUser
+                    // Ir a la siguiente actividad o realizar otras acciones
+                } else {
+                    // Manejar errores
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                }
+            }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ParkingMadridTheme {
-        Greeting("Android")
+    companion object {
+        private const val RC_SIGN_IN = 9001
+        private const val TAG = "MainActivity"
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,4 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
 }
+
