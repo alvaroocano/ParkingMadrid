1254e900ccdb229f877f66862de9ba8a8060607b
Álvaro Cano Sánchez <acanito2003@gmail.com>
2024-04-19 13:23:09 +0200

    Arreglar botón de registro

---
+++ app/src/main/java/com/example/parkingmadrid/ui/theme/NavigationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.parkingmadrid.ui.theme
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.parkingmadrid.R
+
+class NavigationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation)
+    }
+}
