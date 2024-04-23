029c6dbd8561c56f080dbbb04550293bd0929e67
angel <angel16071996@gmail.com>
2024-04-23 19:27:07 +0200

    estilos

--- app/src/main/java/com/example/parkingmadrid/Registro.kt
+++ app/src/main/java/com/example/parkingmadrid/Registro.kt
@@ -1,11 +1,36 @@
 package com.example.parkingmadrid
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.widget.DatePicker
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import java.util.Calendar
 
-class Registro : AppCompatActivity() {
+class Registro : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
+    private lateinit var editTextDOB: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+
+        editTextDOB = findViewById(R.id.editTextDOB)
+
+        editTextDOB.setOnClickListener { showDatePickerDialog() }
+    }
+
+    private fun showDatePickerDialog() {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(this, this, year, month, dayOfMonth)
+        datePickerDialog.show()
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        val selectedDate = String.format("%02d/%02d/%04d", dayOfMonth, month + 1, year)
+        editTextDOB.setText(selectedDate)
     }
 }
