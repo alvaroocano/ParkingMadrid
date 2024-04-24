74ff491611be52b60e600d2db47b02514d885f90
angel <angel16071996@gmail.com>
2024-04-24 19:04:57 +0200

    gif

--- app/src/main/java/com/example/parkingmadrid/Registro.kt
+++ app/src/main/java/com/example/parkingmadrid/Registro.kt
@@ -1,22 +1,57 @@
 package com.example.parkingmadrid
 
 import android.app.DatePickerDialog
+import android.graphics.SurfaceTexture
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.view.Surface
+import android.view.TextureView
 import android.widget.DatePicker
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import java.util.Calendar
+import java.util.*
 
 class Registro : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
+    private lateinit var textureViewBackground: TextureView
+    private lateinit var mediaPlayer: MediaPlayer
     private lateinit var editTextDOB: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
 
-        editTextDOB = findViewById(R.id.editTextDOB)
+        // Inicializar TextureView
+        textureViewBackground = findViewById(R.id.textureViewBackground)
 
+        // Inicializar EditText para la fecha de nacimiento
+        editTextDOB = findViewById(R.id.editTextDOB)
         editTextDOB.setOnClickListener { showDatePickerDialog() }
+
+        // Configurar el listener de SurfaceTexture para el TextureView
+        textureViewBackground.surfaceTextureListener = object : TextureView.SurfaceTextureListener {
+            override fun onSurfaceTextureAvailable(surface: SurfaceTexture, width: Int, height: Int) {
+                val surface = Surface(surface)
+                mediaPlayer = MediaPlayer.create(applicationContext, R.drawable.parking)
+                mediaPlayer.setSurface(surface)
+                mediaPlayer.isLooping = true
+                mediaPlayer.start()
+            }
+
+            override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture, width: Int, height: Int) {}
+
+            override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+                mediaPlayer.stop()
+                mediaPlayer.release()
+                return true
+            }
+
+            override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {}
+        }
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        val selectedDate = String.format("%02d/%02d/%04d", dayOfMonth, month + 1, year)
+        editTextDOB.setText(selectedDate)
     }
 
     private fun showDatePickerDialog() {
@@ -27,10 +62,5 @@ class Registro : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
 
         val datePickerDialog = DatePickerDialog(this, this, year, month, dayOfMonth)
         datePickerDialog.show()
-    }
-
-    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
-        val selectedDate = String.format("%02d/%02d/%04d", dayOfMonth, month + 1, year)
-        editTextDOB.setText(selectedDate)
     }
 }
