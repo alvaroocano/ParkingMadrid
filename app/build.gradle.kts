57c8973dca239c0f4c3b7a913bd76912e8e3d86b
angel <angel16071996@gmail.com>
2024-04-25 21:39:51 +0200

    gif

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ android {
     }
 }
 
+
+
 dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
@@ -68,6 +70,9 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:21.1.0")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("com.github.AppIntro:AppIntro:6.3.1")
+    implementation ("com.github.AppIntro:AppIntro:4.2.3")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/parkingmadrid/NavigationActivity.kt
+++ app/src/main/java/com/example/parkingmadrid/NavigationActivity.kt
@@ -2,10 +2,16 @@ package com.example.parkingmadrid
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.parkingmadrid.Fragmento1
 
 class NavigationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
+
+        // Reemplaza el contenido del contenedor de fragments con tu primer fragmento
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView, Fragmento1())
+            .commit()
     }
 }
